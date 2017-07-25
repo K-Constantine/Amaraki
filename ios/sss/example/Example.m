b73fca6ece7ad2b939dd90bd8ec016a7ace7ae86
Kevin Constantine <conxtantyn@gmail.com>
2017-07-25 13:06:06 +0100

    Ios test project added

---
+++ ios/sss/example/Example.m
@@ -0,0 +1,9 @@
+//
+//  Example.m
+//  example
+//
+//  Created by mac on 7/25/17.
+//  Copyright © 2017 mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/sss/example/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  example
+//
+//  Created by mac on 7/25/17.
+//  Copyright © 2017 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let test: SBTest = SBTest.create()!;
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func clearText(_ sender: Any) {
+        myLabel.text = "";
+    }
+    
+    @IBAction func clickMeButton(_ sender: Any) {
+        
+        myLabel.text = "I am " + test.getName() + " :)";
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
